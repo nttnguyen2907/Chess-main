c83c3769d1d8682af89ed3b249d122acc91a7dc2
nttnguyen2907 <nttnguyen2002@gmail.com>
2024-04-15 09:56:56 +0700

    Initial commit

---
+++ Chess-main/client.py
@@ -0,0 +1,59 @@
+import socket
+
+
+
+host = '127.0.0.1'
+port = 64532
+FORMAT = 'utf8'
+
+def sendList(client, list):
+
+    for item in list:
+        client.sendall(item.encode(FORMAT))
+        # wait response
+        client.recv(1024).decode(FORMAT)
+
+    msg = "end"
+    client.send(msg.encode(FORMAT))
+def client_login(client):
+    account = []
+    username = input('username: ')
+    password = input('password: ')
+
+    # xac thuc user va pass
+
+    account.append(username)
+    account.append(password)
+    sendList(client, account)
+
+    # receive response from server
+    validCheck = client.recv(1024).decode(FORMAT)
+
+    print('validCheck',validCheck)
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("CLIENT SIDE")
+
+    try:
+        client.connect((host, port))
+        print("client address:", client.getsockname())
+        client_name = input('nhap ten ng choi: ')
+        client.sendall(client_name.encode(FORMAT))
+        msg = None
+        while (msg != "x"):
+            msg = input("talk: ")
+            client.sendall(msg.encode(FORMAT))
+
+            # functions called by client
+            if (msg == 'login'):
+                # wait response
+                client.recv(1024)
+                client_login(client)
+
+
+
+
+    except:
+        print("Error")
+
+    client.close()
---
+++ Chess-main/sever.py
@@ -0,0 +1,100 @@
+import socket
+import threading
+import pyodbc
+
+HOST = '127.0.0.1'
+PORT = 64532
+FORMAT ='utf8'
+def handleClient(conn: socket, addr):
+    print("conn: ", conn.getsockname())
+    msg = None
+    while (msg != "x"):
+        msg = conn.recv(1024).decode(FORMAT)
+        print(client_name,addr, ": ", msg)
+
+        if (msg == 'login'):
+            # response
+            conn.sendall(msg.encode(FORMAT))
+            serverLogin(conn)
+
+    print("client", addr, "finished")
+    print(conn.getsockname(), "closed")
+    conn.close()
+
+
+def recvList(conn):
+    list = []
+
+    item = conn.recv(1024).decode(FORMAT)
+
+    while (item != "end"):
+        list.append(item)
+        # response
+        conn.sendall(item.encode(FORMAT))
+        item = conn.recv(1024).decode(FORMAT)
+
+    return list
+
+def serverLogin(conn):
+    # recv account from client
+    client_account = recvList(conn)
+    print(client_account)
+    # query data: password
+    cursor.execute("select password from Account where username = ?", client_account[0])
+    password = cursor.fetchone()
+    data_password = password[0]
+    print("'"+data_password+"'")
+
+    msg = "ok"
+    if (client_account[1] == data_password):
+        msg = "Login successfully"
+        print(msg)
+
+
+    else:
+        msg = "Invalid password"
+        print(msg)
+
+    conn.sendall(msg.encode(FORMAT))
+
+
+
+if __name__ == '__main__':
+    conx = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server}; SERVER=MSI\SQLEXPRESS;Database=Chess_Account;UID=tn;PwD=123;')
+    cursor = conx.cursor()
+    print('danh sách tài khoản')
+    for user in cursor.execute('select * from Account'):
+        print(user.username,user.password)
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+    print("\n SERVER ne ")
+    print(HOST, PORT)
+    print('waiting for client ...')
+    nClient = 0
+    clients = {}  # Tạo một từ điển để lưu trữ thông tin về client
+    while (nClient < 3):
+        try:
+            conn, addr = s.accept()
+
+            # Nhận tên của client từ socket
+            client_name = conn.recv(1024).decode(FORMAT)
+
+            # Thêm client vào từ điển
+            clients[addr] = client_name
+            thr = threading.Thread(target=handleClient, args=(conn, addr))
+            thr.daemon = False
+            thr.start()
+
+        except:
+            print("Error")
+
+        nClient += 1
+
+    print("End")
+    s.close()
+    conx.close()
+
